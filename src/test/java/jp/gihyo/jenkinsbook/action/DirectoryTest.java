ac85aef993dd43e3c162cd3f8eaefca7dac1fd48
tsuchiyamak <tsuchiyamak@192.168.0.105>
2018-09-17 18:01:59 +0900

    Jenkinsでのファイルの作成テストの実行を確認するため。

--- src/test/java/jp/gihyo/jenkinsbook/action/DirectoryTest.java
+++ src/test/java/jp/gihyo/jenkinsbook/action/DirectoryTest.java
@@ -1,5 +1,9 @@
 package jp.gihyo.jenkinsbook.action;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 import org.junit.Test;
 
 public class DirectoryTest {
@@ -8,6 +12,20 @@ public class DirectoryTest {
 	public void test() {
 		String currentDir = System.getProperty("user.dir");
 		System.out.println("currentDir: " + currentDir);
+	}
+	
+	@Test
+	public void ファイル作成のテスト() {
+		try (
+				FileWriter out = new FileWriter(new File("./src/test/resources/output.txt"));
+				) {
+			out.write("(1) output.txt");
+			out.write("(2) output.txt");
+			out.write(System.lineSeparator());
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
